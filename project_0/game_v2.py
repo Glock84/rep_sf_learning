353b57cb44f7aa85ee7b80ce60f001250af48037
Glock84 <glock84tnt@gmail.com>
2022-05-18 15:17:53 +0300

    update

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-def random_predict(number:int=1)->int:
+def random_predict(number:int = 1 ) -> int:
     """Рандомно угадываем число
 
     Args:
@@ -32,7 +32,7 @@ def score_game(random_predict) -> int:
     
     count_ls = [] # список для сохранения кол-ва попыток
     np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+    random_array = np.random.randint(1, 101, size=(100)) # загадали список чисел
     
     for number in random_array:
         count_ls.append(random_predict(number))
@@ -42,6 +42,6 @@ def score_game(random_predict) -> int:
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
-#RUN
-if __name__ == '__main':
+# RUN
+if __name__ == '__main__':
     score_game(random_predict)
